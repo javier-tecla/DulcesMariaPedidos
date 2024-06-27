9485d185fc1234d4c8bf25bc27a6748810eec9b4
javierBorjas <cristman11@gmail.com>
2024-06-27 00:42:50 -0300

    Modificando el button de login y register

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,5 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
     plugins: [forms],
 };
