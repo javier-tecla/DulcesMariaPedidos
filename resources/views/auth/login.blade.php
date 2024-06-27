92d4ea174c4327b2aba57f07598c79ebbe524cde
javierBorjas <cristman11@gmail.com>
2024-06-27 01:00:46 -0300

    Modificando el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,21 +27,26 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-rose-500 shadow-sm focus:ring-rose-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="flex justify-between my-5">
+            <x-link
+                :href="route('register')"
+            >
+                {{ __('Crear Cuenta') }}
+            </x-link>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <x-link
+                :href="route('password.request')"
+            >
+                {{ __('Olvidaste tu Password') }}
+            </x-link>
         </div>
+            <x-primary-button class="w-full justify-center">
+                {{ __('Iniciar Sesión') }}
+            </x-primary-button>
     </form>
 </x-guest-layout>
