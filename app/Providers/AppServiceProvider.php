0af025ca7f9400e00f4d0a4b89cdd8192ce4a2a6
javierBorjas <cristman11@gmail.com>
2024-06-27 01:37:01 -0300

    Personalizar el email que se envia (lo deje comentado como referencia)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // VerifyEmail::toMailUsing(function($notifiable, $url) {
+        //     return (new MailMessage)
+        //         ->greeting('Hola!')
+        //         ->subject('Verificar Cuenta')
+        //         ->line('Tu Cuenta ya esta casi lista, solo debes presionar en el enlace a continuación.')
+        //         ->action('Confirmar Cuenta', $url)
+        //         ->line('Si no creaste esta cuenta, puedes ignorar este mensaje.')
+        //         ->salutation('Saludos, Dulces Maria.');
+        // });
     }
 }
