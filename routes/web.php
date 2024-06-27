6126876bd42bdb98513ec30abd2a4e2aa321565b
javierBorjas <cristman11@gmail.com>
2024-06-27 02:09:08 -0300

    Modificando el Layout Principal

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [PedidoController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [PedidoController::class, 'index'])->middleware(['auth', 'verified'])->name('pedidos.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
