52c019d26224639b5f149212a91ca57a74133dbe
javierBorjas <cristman11@gmail.com>
2024-07-01 11:34:18 -0300

    Creando el HomeController, creando la vista de home.index, creando la ruta y agregando la img de home

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <div class="py-16 bg-gray-50 overflow-hidden lg:py-24">
+        <div class=" max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+            <div class="relative">
+                <h2 class="text-center text-4xl leading-8 font-medium tracking-tight text-rose-400 sm:text-5xl">Donde los Sueños Dulces se Hacen Realidad</h2>
+                <div class="py-8 mt-8 mx-auto max-w-sm">
+                    <img src="/assets/imagen_principal.jpeg" alt="Imagen descriptiva de tus pasteles" class="mx-auto rounded-lg shadow-lg">
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class )->name('home');
             
 Route::get('/dashboard', [PedidoController::class, 'index'])->middleware(['auth', 'verified'])->name('pedidos.index');
 Route::get('/pedidos/create', [PedidoController::class, 'create'])->middleware(['auth', 'verified'])->name('pedidos.create');
