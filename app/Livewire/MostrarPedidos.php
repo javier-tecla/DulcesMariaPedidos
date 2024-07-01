232f35b5b3ae73230377d95915ff706a1833c05a
javierBorjas <cristman11@gmail.com>
2024-07-01 16:53:44 -0300

    modificando para que se elimine el pedido en produccion

--- app/Livewire/MostrarPedidos.php
+++ app/Livewire/MostrarPedidos.php
@@ -4,15 +4,24 @@ namespace App\Livewire;
 
 use App\Models\Pedido;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class MostrarPedidos extends Component
 {
+    use WithPagination;
+
     protected $listeners = ['eliminarPedido'];
 
    public function eliminarPedido($pedidoId)
    {
         $pedido = Pedido::findOrFail($pedidoId);
         $pedido->delete();
+
+         // Mostrar un mensaje de confirmación
+         session()->flash('message', 'Pedido eliminado correctamente.');
+
+         // Actualizar la lista de pedidos después de eliminar
+         $this->resetPage(); // Reinicia la página actual a la primera
    }
 
     public function render()
